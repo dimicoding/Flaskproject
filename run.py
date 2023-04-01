1536dfae671f2df125169b22b32746998be64011
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-04-01 18:26:02 +0000

    Playing with routing forms in flask for contact.html

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -19,13 +22,30 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<details_name>")
+def about_member(details_name):
+    details = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == details_name:
+                details = obj
+        return render_template("details.html", details=details)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have recieved your message!".format(
+            request.form["name"]))
     return render_template("contact.html", page_title="Contact")
+
 
 
 @app.route("/careers")
 def careers():
+    if request.method == "POST":
+        print(request.form)
     return render_template("careers.html", page_title="Careers")
 
 
